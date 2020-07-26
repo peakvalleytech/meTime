3d7fe1df4b02a105ce75f9f179b28ca72ff12e41
typelogical <williamng10407@hotmail.com>
2020-07-25 23:17:50 -0700

    Add tests for MediationViewModel.

--- app/src/test/java/peakvalleytech/neverdown/GratitudeDaoTest.kt
+++ app/src/test/java/peakvalleytech/neverdown/GratitudeDaoTest.kt
@@ -1,5 +1,7 @@
 package peakvalleytech.neverdown
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -7,21 +9,39 @@ import org.junit.After
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
+import peakvalleytech.neverdown.data.local.NeverDownDatabase
+import peakvalleytech.neverdown.model.gratitude.GratitudeItem
 
 
 @RunWith(AndroidJUnit4::class)
 class GratitudeDaoTest {
-    //    private lateinit var mDatabase: NeverDownDatabase
+    val instantTaskExecutorRule = InstantTaskExecutorRule()
+    private lateinit var mDatabase: NeverDownDatabase
+
     @Before
     fun init() {
-//        mDatabase = Room.inMemoryDatabaseBuilder(
-//            ApplicationProvider.getApplicationContext(),
-//            NeverDownDatabase::class.java
-//        ).build()
+        mDatabase = Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            NeverDownDatabase::class.java
+        ).build()
     }
 
     @Test
     fun insertAndSelectItems() {
+        val gratitudeDao = mDatabase.gratitudeItemDao()
+        val itemNames = listOf("Place to sleep", "Food", "A job", "Water", "Clothes", "Computer")
+        val items: MutableList<GratitudeItem> = mutableListOf()
+        var id = 1
+        for (itemName in itemNames) {
+            val item = GratitudeItem(id++, itemName)
+            items.add(item)
+        }
+        val observer = Observer<List<GratitudeItem>> {}
 
+        try {
+//            gratitudeDao.getAllItems()
+        } finally {
+
+        }
     }
 }
--- app/src/test/java/peakvalleytech/neverdown/meditation/MeditationViewModelTest.kt
+++ app/src/test/java/peakvalleytech/neverdown/meditation/MeditationViewModelTest.kt
@@ -1,5 +1,74 @@
-package peakvalleytech.neverdown.ui.activity.meditation
+package peakvalleytech.neverdown.meditation
 
-import androidx.arch.core.executor.
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.not
+import org.junit.Before
+import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.Test
+import peakvalleytech.neverdown.ui.activity.meditation.MeditationViewModel
+
 class MeditationViewModelTest {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    private lateinit var meditationViewModel: MeditationViewModel
+
+    @Before
+    fun setUp() {
+        meditationViewModel = MeditationViewModel()
+    }
+
+    @Test
+    fun counter_shouldStartAtOne() {
+        val observer = Observer<Int> {}
+        try {
+            meditationViewModel.mBreathCounter.observeForever(observer)
+            val counter = meditationViewModel.mBreathCounter.value
+            assertThat(counter, `is`(1))
+        } finally {
+            meditationViewModel.mBreathCounter.removeObserver(observer)
+        }
+    }
+
+    @Test
+    fun counter_shouldUpdateEverySecond() {
+
+    }
+
+    @Test
+    fun counter_shouldIncrementByOne() {
+
+    }
+
+    @Test
+    fun counter_shouldEndOnFour() {
+
+    }
+
+    @Test
+    fun counter_shouldRestartAfterLimitReached() {
+
+    }
+
+    @Test
+    fun state_shouldStartOnInhale() {
+
+    }
+
+    @Test
+    fun state_shouldUpdateToExhaleOnCounterLimit() {
+
+    }
+
+    @Test
+    fun state_shouldUpdateToInhaleOnCounterRestart() {
+
+    }
+
+
+
+
 }
