e0d75c6087ef73761ae8521f651f921eaa4a5c9e
typelogical <williamng10407@hotmail.com>
2021-01-16 21:04:45 -0800

    Update pretotype.

---
+++ app/src/main/java/peakvalleytech/neverdown/ui/activity/pictures/PicturesFragment.kt
@@ -0,0 +1,38 @@
+package peakvalleytech.neverdown.ui.activity.pictures
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil.inflate
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import peakvalleytech.neverdown.NeverDownApplication
+import peakvalleytech.neverdown.R
+import peakvalleytech.neverdown.databinding.FragmentGratitudeBinding
+
+class PicturesFragment : Fragment() {
+    private val viewModel by viewModels<PicturesViewModel> {
+        PicturesViewModelFactory((requireContext().applicationContext as NeverDownApplication).gratitudeRepository)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val binding = inflate<FragmentGratitudeBinding>(
+            inflater,
+            R.layout.fragment_gratitude,
+            container,
+            false
+        )
+        binding.gratitudeViewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+        viewModel.mItems.observe(viewLifecycleOwner, Observer {
+            viewModel.updateCurrentItem()
+        })
+
+        return binding.root
+    }
+}
---
+++ app/src/main/java/peakvalleytech/neverdown/ui/activity/pictures/PicturesViewModel.kt
@@ -0,0 +1,78 @@
+package peakvalleytech.neverdown.ui.activity.pictures
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import peakvalleytech.neverdown.data.repo.GratitudeRepository
+import peakvalleytech.neverdown.model.gratitude.GratitudeItem
+import kotlin.random.Random
+
+class PicturesViewModel(
+    private val gratitudeRepository : GratitudeRepository
+) : ViewModel() {
+    /**
+     * The gratitude items that the app selects from
+     */
+    private var _mItems = gratitudeRepository.getItems()
+    val mItems: LiveData<List<GratitudeItem>> = _mItems
+
+    /**
+     * Indciates whether the user is grateful for the currently shown item
+     */
+    private val _mIsGrateful = MutableLiveData<Boolean>()
+    val mIsGrateful = _mIsGrateful
+
+    /**
+     * The currently displayed item
+     */
+    private val _mItem = MutableLiveData<GratitudeItem>()
+    val mItem : LiveData<GratitudeItem> = _mItem
+
+    /**
+     * Keep track of which items have already been shown to avoid showing the same item
+     * twice per session
+     */
+    private var seenItems : MutableList<GratitudeItem> = mutableListOf()
+
+    fun loadInitialItem() {
+//        viewModelScope.launch {
+//            _mItems = gratitudeRepository.getItems()
+//            mItems = _mItems
+//            _mItem.value = mItems.get(0)
+//            println("items : " + mItems)
+//        }
+    }
+
+    /**
+     * Select a new item from the items list and set it to current item
+     */
+    fun updateCurrentItem() {
+        val items = mItems.value
+        var index = rand(0, items?.size as Int)
+        _mItem.value = items.get(index)
+    }
+    fun isGrateful(boolean: Boolean) {
+        _mIsGrateful.value = boolean
+    }
+
+    private val random = Random
+
+    fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
+    }
+
+    fun loadItems() {
+//        TODO("Not yet implemented")'
+
+
+    }
+}
+
+@Suppress("UNCHECKED_CAST")
+class PicturesViewModelFactory (
+    private val gratitudeRepository: GratitudeRepository
+): ViewModelProvider.NewInstanceFactory() {
+    override fun <T : ViewModel> create(modelClass: Class<T>) =
+        (PicturesViewModel(gratitudeRepository) as T)
+}
